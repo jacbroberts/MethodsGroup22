f296d2387c958a91bab32495d4dd057dd75c1f0d
RileyDThrasher <rileydthrasher@gmail.com>
2021-11-19 13:52:41 -0600

    progress

--- main.cc
+++ main.cc
@@ -4,7 +4,31 @@ using namespace std;
 
 int main()
  {
-  cout<<"hola";
-  cout<<"Welcome to the T-shirt store!\n";
-  cout<<"We sell T-shirts.\n";
+  string answer;
+  cout<<"Hello! Welcome to our T-shirt shop.\n";
+  cout<<"We have a variety of Brands and colors at cheap prices to choose from.\n";
+  cout<<"Do you have an account with us? ";
+  cin>>answer;
+    if (answer == "no")
+     {
+        string b;
+        cout<<"Would you like to create an account? ";
+        cin>>b;
+        if (b == "no")
+        {
+        return 0;
+        }
+        if (b == "yes")
+        {
+        string username;
+        cout<<"Splendid!\n";
+        cout<<"Enter new username: ";
+        cin>>username;
+        }
+     }
+    // rt: Yes I do have an account
+    if (answer == "yes")
+     {
+        cout<<"Login";
+     }
  }
