5059ba7064bec5fb2bae94dba2305a6036b2607f
RileyDThrasher <rileydthrasher@gmail.com>
2021-11-30 11:55:53 -0600

    Update to main.cc
    Account info checks, pretty up and move to cart

--- main.cc
+++ main.cc
@@ -12,12 +12,16 @@ void Login()
  {
   string LoginUsername;
   string LoginPassword;
+  // rt: username
   cout<<"Great, let's get you logged in.\n";
   cout<<"Enter username: ";
   cin>>LoginUsername;
+  // rt: EXIT
   if(LoginUsername == "EXIT") {goodbye();}
+  // rt: password
   cout<<"Enter password: ";
   cin>>LoginPassword;
+  // rt: EXIT
   if(LoginPassword == "EXIT") {goodbye();}
  }
 
@@ -49,31 +53,127 @@ int main()
         // rt: if yes i do want to create an account
         if (b == "y")
          {
+          // rt: Create username
           string username;
           cout<<"Great, let's make a new account.\n";
           cout<<"Enter new username: ";
           cin>>username;
           // rt: EXIT
           if (username == "EXIT") {goodbye();}
-
+         
+          // rt: Create password
           string password;
           cout<< "Enter password: ";
           cin>>password;
+          // rt: EXIT
           if (password == "EXIT") {goodbye();}
+          
+          // rt: move to login
+          Login();
         }
         // rt: EXIT
         if (b == "EXIT") {goodbye();}
      }
+    
     // rt: Yes I do have an account
     if (answer == "y"){ Login();}
 
     // rt: EXIT
     if (answer == "EXIT") {goodbye();}
     
-    // rt: BACK
-    if (answer == "BACK")
+    // rt: Welcome user what you wanna do
+    string MainMenuOption;
+    cout<<"Welcome " << username << " what would you like to do?\n\n";
+    cout<<"Enter: ACCOUNTINFO\n";
+    cout<<"To see information about your account\n\n";
+    cout<<"Enter: CART\n";
+    cout<<"To see your carts info\n\n";
+    cout<<"Enter: INVENTORY\n";
+    cout<<"To browse our inventory\n\n";
+    cin>>MainMenuOption;
+
+    // rt: ACCOUNTINFO
+    if (MainMenuOption == "ACCOUNTINFO")
      {
-      
+       string AccountInfoOption;
+       cout<<"This is where you can see information about your account\n\n";
+       cout<<"Enter: DETAILS\n";
+       cout<<"To see your account\n\n";
+       cout<<"Enter: VIEWSHIPPING\n";
+       cout<<"To see your shipping information\n\n";
+       cout<<"Enter: EDITSHIPPING\n";
+       cout<<"To edit your shipping information\n\n";
+       cout<<"Enter: VIEWPAYINFO\n";
+       cout<<"To see your payment information\n\n";
+       cout<<"Enter: EDITPAYINFO\n";
+       cout<<"To edit your payment information\n\n";
+       cout<<"Enter: HISTORY\n";
+       cout<<"To view order history\n\n";
+       cout<<"Enter: LOGOUT\n";
+       cout<<"To logout of your account\n";
+       cout<<"Enter: DELETE\n";
+       cout<<"To delete you account";
+       cin>>AccountInfoOption;
+
+       if (AccountInfoOption == "DETAILS")
+        {
+         // rt: go to details function
+        }
+
+       if (AccountInfoOption == "VIEWSHIPPING")
+        {
+         // rt: go to viewshipping information function
+        }
+       
+       if (AccountInfoOption == "EDITSHIPPING")
+        {
+         // rt: go to edit shipping information function
+        }
+
+       if (AccountInfoOption == "VIEWPAYINFO")
+        {
+         // rt: go to view payment information function
+        }
+
+       if (AccountInfoOption == "EDITPAYINFO")
+        {
+         // rt: go to edit payment information function
+        }
+
+       if (AccountInfoOption == "HISTORY")
+        {
+         // rt: go to history function
+        }
+       
+       if (AccountInfoOption == "LOGOUT")
+        {
+         // rt: go to logout function
+         // rt: basically back to welcome? or just exit?
+        }
+        
+        // rt: go to delete account function
+       if (AccountInfoOption == "DELETE")
+        {
+
+        }
+
+       // rt: EXIT
+       if(AccountInfoOption == "EXIT"){goodbye();}
+
      }
     
+    // rt: CART
+    if (MainMenuOption == "CART")
+     {
+    
+     }
+    
+    // rt: INVENTORY
+    if (MainMenuOption == "INVENTORY")
+     {
+    
+     }
+    
+    // rt: EXIT
+    if (MainMenuOption == "EXIT") {goodbye();}
  }
