67f45cbe84879f9d0af80d3f0c1ae1ef6b608e5e
David Reaves <jdreaves18@gmail.com>
2021-11-30 16:29:15 -0600

    inventory

--- inventory.h
+++ inventory.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <string>
+
+using std::string;
+
+class Inventory
+{
+private:
+	string itemID, itemBrand, itemType;
+	int itemCount, itemPrice;
+
+public:
+	Inventory();
+	int DecrementStock(int purchased);
+	bool IsAvailable();
+	string GetBrand();
+	string GetType();
+	string GetID();
+	int GetCount();
+	int GetPrice();
+	void SetBrand(string brand);
+	void SetType(string type);
+	void SetID(string id);
+	void SetCount(int count);
+	void SetPrice(int price);
+};
