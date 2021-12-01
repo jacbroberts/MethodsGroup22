2b3d061667a0b58f259d08dc1ba546ac0c333609
Riley Thrasher <83115611+RileyDThrasher@users.noreply.github.com>
2021-11-30 18:04:16 -0600

    Revert "Jdr"

--- main.cc
+++ main.cc
@@ -4,160 +4,6 @@
 using namespace std;
 
 void goodbye()
-{
-    cout << "\nJust T-Shirts says GOODBYE!\n";
-    exit(EXIT_FAILURE);
-}
-
-void Login()
-{
-    string LoginUsername;
-    string LoginPassword;
-    // rt: username
-    cout << "\nGreat, let's get you logged in.\n";
-    cout << "Enter username: ";
-    cin >> LoginUsername;
-    // rt: EXIT
-    if (LoginUsername == "EXIT") { goodbye(); }
-    // rt: password
-    cout << "Enter password: ";
-    cin >> LoginPassword;
-    // rt: EXIT
-    if (LoginPassword == "EXIT") { goodbye(); }
-}
-
-int main()
-{
-    // jdr: Initializing the Inventory
-    Inventory Gucci, Walmart, State;
-    string id, stock;
-    int count;
-    ifstream invinit;
-    invinit.open("inventory.txt");
-    if (invinit.is_open())
-    {
-        getline(invinit, id);
-        getline(invinit, stock);
-        count = stoi(stock);
-        Gucci.SetBrand("Gucci");
-        Gucci.SetType("T-Shirt");
-        Gucci.SetID(id);
-        Gucci.SetPrice(169);
-        Gucci.SetCount(count);
-
-        getline(invinit, id);
-        getline(invinit, stock);
-        count = stoi(stock);
-        Walmart.SetBrand("Walmart");
-        Walmart.SetType("T-Shirt");
-        Walmart.SetID(id);
-        Walmart.SetPrice(6);
-        Walmart.SetCount(count);
-
-        getline(invinit, id);
-        getline(invinit, stock);
-        count = stoi(stock);
-        State.SetBrand("Hail State");
-        State.SetType("T-shirt");
-        State.SetID(id);
-        State.SetPrice(69);
-        State.SetCount(count);
-
-        invinit.close();
-    }
-    else
-    {
-        cout << "System could not load properly.\n";
-    }
-
-    // rt: Welcome
-    string answer;
-    int cont;
-    int conttwo;
-    while (answer != "y" && answer != "n")
-    {
-        cout << "##############################################";
-        cout << "\nHello! Welcome to our T-shirt shop.\n\n";
-        cout << "We only have T-shirts and the best T-shirts.\n\n";
-        cout << "Enter: EXIT\n";
-        cout << "To leave the site anytime\n\n";
-        cout << "Enter: BACK\n";
-        cout << "To go back a page anytime\n\n";
-        cout << "Do you have an account with us(y/n)? ";
-        cin >> answer;
-    }
-    // rt: No, I do NOT have an account
-    if (answer == "n")
-    {
-        string b;
-        while (b != "y" && b != "n")
-        {
-            cout << "\nYou must have an account to proceed.\n";
-            cout << "Would you like to create an account(y/n)? ";
-            cin >> b;
-        }
-        // rt: if no i dont want to create an account
-        if (b == "n")
-        {
-            cout << "\nYou must have an account to proceed.\n";
-            goodbye();
-        }
-        // rt: if yes i do want to create an account
-        if (b == "y")
-        {
-            // rt: Create username
-            string username;
-            cout << "Great, let's make a new account.\n";
-            cout << "Enter new username: ";
-            cin >> username;
-            // rt: EXIT
-            if (username == "EXIT") { goodbye(); }
-
-            // rt: Create password
-            string password;
-            cout << "Enter password: ";
-            cin >> password;
-            // rt: EXIT
-            if (password == "EXIT") { goodbye(); }
-
-            // rt: move to login
-            Login();
-            cont = 1;
-        }
-        // rt: EXIT
-        if (b == "EXIT") { goodbye(); }
-    }
-
-    // rt: Yes I do have an account
-    if (answer == "y")
-    {
-        Login();
-        cont = 1;
-    }
-
-    // rt: EXIT
-    if (answer == "EXIT") { goodbye(); }
-
-    if (cont == 1)
-    {
-        conttwo = 1;
-        while (conttwo == 1)
-        {
-            string MainMenuOption;
-            while (MainMenuOption != "ACCOUNTINFO" && MainMenuOption != "CART" && MainMenuOption != "INVENTORY" && MainMenuOption != "EXIT")
-            {
-                // rt: Welcome user what you wanna do
-                cout << "##############################################";
-                cout << "\nWelcome! What would you like to do?\n\n";
-                cout << "Enter: ACCOUNTINFO\n";
-                cout << "To see information about your account\n\n";
-                cout << "Enter: CART\n";
-                cout << "To see your carts info\n\n";
-                cout << "Enter: INVENTORY\n";
-                cout << "To browse our inventory\n\n";
-                cout << "Select an option: ";
-                cin >> MainMenuOption;
-                cout << "\n";
  {
    cout<<"\nJust T-Shirts says GOODBYE!\n";
    exit(EXIT_FAILURE);
@@ -274,44 +120,6 @@ int main()
                 // rt: ACCOUNTINFO
                 if (MainMenuOption == "ACCOUNTINFO")
                 {
-                    string AccountInfoOption;
-                    while (AccountInfoOption != "DETAILS" && AccountInfoOption != "VIEWSHIPPING" && AccountInfoOption != "EDITSHIPPING" && AccountInfoOption != "VIEWPAYINFO" && AccountInfoOption != "EDITPAYINFO" && AccountInfoOption != "HISTORY" && AccountInfoOption != "LOGOUT" && AccountInfoOption != "DELETE" && AccountInfoOption != "EXIT")
-                    {
-                        cout << "##############################################";
-                        cout << "\nThis is where you can see information about your account\n\n";
-                        cout << "Enter: DETAILS\n";
-                        cout << "To see your account\n\n";
-                        cout << "Enter: VIEWSHIPPING\n";
-                        cout << "To see your shipping information\n\n";
-                        cout << "Enter: EDITSHIPPING\n";
-                        cout << "To edit your shipping information\n\n";
-                        cout << "Enter: VIEWPAYINFO\n";
-                        cout << "To see your payment information\n\n";
-                        cout << "Enter: EDITPAYINFO\n";
-                        cout << "To edit your payment information\n\n";
-                        cout << "Enter: HISTORY\n";
-                        cout << "To view order history\n\n";
-                        cout << "Enter: LOGOUT\n";
-                        cout << "To logout of your account\n";
-                        cout << "Enter: DELETE\n";
-                        cout << "To delete you account\n\n";
-                        cout << "Select an option: ";
-                        cin >> AccountInfoOption;
-
-                        // rt: go to details function
-                        if (AccountInfoOption == "DETAILS")
-                        {
-
-                        }
-
-                        // rt: go to viewshipping information function
-                        if (AccountInfoOption == "VIEWSHIPPING")
-                        {
-
-                        }
-
-                        // rt: go to edit shipping information function
-                        if (AccountInfoOption == "EDITSHIPPING")
                 string AccountInfoOption;
                 while(AccountInfoOption != "DETAILS" && AccountInfoOption != "VIEWSHIPPING" && AccountInfoOption != "EDITSHIPPING" && AccountInfoOption != "VIEWPAYINFO" && AccountInfoOption != "EDITPAYINFO" && AccountInfoOption != "HISTORY" && AccountInfoOption != "LOGOUT" && AccountInfoOption != "DELETE" && AccountInfoOption != "EXIT")
                 {
@@ -354,10 +162,6 @@ int main()
 
                         }
 
-
-                        // rt: go to view payment information function
-                        if (AccountInfoOption == "VIEWPAYINFO")
-
                     // rt: go to view payment information function
                     if (AccountInfoOption == "VIEWPAYINFO")
                         {
@@ -366,7 +170,6 @@ int main()
 
                     // rt: go to edit payment information function
                     if (AccountInfoOption == "EDITPAYINFO")
-
                         {
 
                         }
@@ -401,7 +204,6 @@ int main()
                 // rt: CART
                 if (MainMenuOption == "CART")
                 {
-
                 string CartOption;
                 while(CartOption != "CART" && CartOption != "REMOVEITEM" && CartOption != "ADDITEM" && CartOption != "QUANTITY" && CartOption != "CHECKOUT" && CartOption != "EXIT")
                  {
@@ -449,7 +251,7 @@ int main()
                         {
 
                         }
-      
+
                     // rt: BACK
                     if(CartOption == "BACK"){}
 
@@ -494,4 +296,3 @@ int main()
     }
     return 0;
  }
-
